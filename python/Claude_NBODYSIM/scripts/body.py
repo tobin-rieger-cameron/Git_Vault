5fabc73d48c22ab37c5507ca46915a587944af88
tizz <tobincameron@gmail.com>
2026-04-20 15:43:20 +0000

    added unique identifiers to each body

--- python/Claude_NBODYSIM/scripts/body.py
+++ python/Claude_NBODYSIM/scripts/body.py
@@ -1,5 +1,6 @@
 # scripts/body.py
 
+import uuid
 from utils.colors import *
 from utils.vector3D import Vector3D
 
@@ -79,6 +80,7 @@ class Body:
         trail_color,
     ):
         self.name = name
+        self.id       = str(uuid/uuid4())
         self.position = position
         self.velocity = velocity
         self.mass = mass
