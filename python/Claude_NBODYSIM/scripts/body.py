8313cc52410d4e50bbc07ff3358b7c52b9aed25b
tizz <tobincameron@gmail.com>
2026-04-20 20:21:31 +0000

    switched / to . for self.id

--- python/Claude_NBODYSIM/scripts/body.py
+++ python/Claude_NBODYSIM/scripts/body.py
@@ -80,7 +80,7 @@ class Body:
         trail_color,
     ):
         self.name = name
-        self.id       = str(uuid/uuid4())
+        self.id       = str(uuid.uuid4())
         self.position = position
         self.velocity = velocity
         self.mass = mass
